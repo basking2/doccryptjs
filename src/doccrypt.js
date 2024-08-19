121c9bcc947c2314d58560248575594fd2f562dd
Sam Baskinger <basking2@yahoo.com>
2024-08-19 17:37:05 -0500

    doccrypt.js: Implement basic crypto use.

--- src/doccrypt.js
+++ src/doccrypt.js
@@ -1,5 +1,60 @@
-class DocCrypt {
-    constructor(parameters) {
+crypto = require ('node:crypto')
 
+class DocCrypt {
+    constructor(parameters = {}) {
+        this.keylength = parameters['keylength'] || 32
+        this.ivlength = parameters['ivlength'] || 16
+        this.algorithm = parameters['algorithm'] || 'aes-256-cbc'
+        this.password = parameters['password']
+        this.salt = parameters['salt'] || 'salt'
     }
+
+    async key() {
+        return new Promise((resolve, reject) => {
+            crypto.scrypt(this.password, this.salt, this.keylength, (err, buff) => {
+                if (err) {
+                    return reject(err)
+                } 
+
+                resolve(buff)
+            })
+        })
+    }
+
+    async iv() {
+        return new Promise((resolve, reject) => {
+            crypto.randomFill(new Uint8Array(this.ivlength), (err, iv) => {
+                if (err) {
+                    return reject(err)
+                }
+
+                resolve(iv)
+            })    
+        })
+    }
+
+    async encrypt(text, encoding = 'hex') {
+        const key = await this.key()
+        const iv = await this.iv()
+        const cipher = crypto.createCipheriv(this.algorithm, key, iv)
+
+        let ciphertext = cipher.update(text, 'utf8', encoding)
+        ciphertext += cipher.final(encoding)
+        return Promise.resolve({ciphertext, key, iv, encoding, 'salt': this.salt})
+    }
+
+    async decrypt(obj) {
+
+        const key = obj.key || await this.key()
+        const iv = obj.iv
+
+        const decipher = crypto.createDecipheriv(this.algorithm, key, iv)
+        let decrypted = decipher.update(obj.ciphertext, obj.encoding, 'utf8')
+        decrypted += decipher.final('utf8')
+        return Promise.resolve(decrypted)
+    }
+}
+
+module.exports = {
+    DocCrypt
 }
--- src/doccrypt.test.js
+++ src/doccrypt.test.js
@@ -1,7 +1,30 @@
+const doccrypt = require('./doccrypt')
+
 
 test('ok', () => {
     expect(1).toBe(1)
 });
   
-  
-  
+test('2k key', async () => {
+    const dc = new doccrypt.DocCrypt({'password': 'hi', 'salt': 'moo', 'keylength': 256})
+    const key = await dc.key()
+    expect(key).not.toBeNull()
+    expect(key.length).toBe(256)
+})
+
+test('32 byte iv', async () => {
+    const dc = new doccrypt.DocCrypt({'ivlength': 32})
+    const iv = await dc.iv()
+    expect(iv).not.toBeNull()
+    expect(iv.length).toBe(32)
+})
+
+test('cipher', async () => {
+    const dc = new doccrypt.DocCrypt({'password': 'pw'})
+    const enc = await dc.encrypt("test")
+    console.log(enc)
+    const denc = await dc.decrypt(enc)
+    console.log(denc)
+
+    expect('test').toEqual(denc)
+})
