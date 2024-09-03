3cf6e5e82d9bf306b900ce1116bd94813548db85
Sam Baskinger <basking2@yahoo.com>
2024-09-02 20:43:19 -0500

    Use webpack.ProvidePlugin to provide process.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
 const path = require('path');
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV == 'production';
@@ -20,6 +21,10 @@ const config = {
         new HtmlWebpackPlugin({
             template: 'index.html',
         }),
+
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+        })
 
         // Add your plugins here
         // Learn more about plugins from https://webpack.js.org/configuration/plugins/
